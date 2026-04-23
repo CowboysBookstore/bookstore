452860975123528afe02111444d7f2355029d5cc
Sandrin Muramutsa <sandrinmuramutsa@gmail.com>
2026-04-23 15:20:27 -0500

    Update superuser email to admin@mcneese.edu

--- backend/create_superuser.sh
+++ backend/create_superuser.sh
@@ -2,5 +2,5 @@
 # exit on error
 set -o errexit
 
-python manage.py createsuperuser --noinput --username "admin" --email "admin@bookstore.com" || true
+python manage.py createsuperuser --noinput --username "admin" --email "admin@mcneese.edu" || true
 (echo "admin123"; echo "admin123") | python manage.py changepassword "admin" || true
